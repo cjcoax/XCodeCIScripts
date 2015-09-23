e8a59437af1c423e96d4f28499eb34d822e8ed81
Amir Rezvani <arezvani@babycemter.com>
2015-09-23 11:19:10 -0700

    under progress

--- hockey.rb
+++ hockey.rb
@@ -4,15 +4,24 @@ def ipa_path
  File.join(ENV['XCS_OUTPUT_DIR'], ENV['BC_PRODUCT_NAME']) + ".ipa"
 end
 
-print ipa_path 
-
-
 def bot_name
   ENV['XCS_BOT_NAME']
 end
 
+
 def product_name
-  ENV['CMD_PRODUCT_NAME']
+  ENV['BC_PRODUCT_NAME']
 end
 
+def hockey_token
+  ENV['BC_HOCKEY_TOKEN']
+end
 
+def dsym_path_before_zip
+  File.join(ENV['XCS_ARCHIVE'], 'dSYMs', "#{product_name}.app.dSYM")
+end
+
+puts bot_name
+puts ipa_path 
+puts hockey_token
+puts dsym_path_before_zip
