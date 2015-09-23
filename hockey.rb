518f72c6efc5587dbb44b717861dfca0c5d2c6a7
Amir Rezvani <arezvani@babycemter.com>
2015-09-23 15:51:40 -0700

    more progress

--- hockey.rb
+++ hockey.rb
@@ -4,54 +4,57 @@ def ipa_path
  File.join(ENV['XCS_OUTPUT_DIR'], ENV['BC_PRODUCT_NAME']) + ".ipa"
 end
 
-def bot_name
-  ENV['XCS_BOT_NAME']
-end
-
-
-def product_name
-  ENV['BC_PRODUCT_NAME']
-end
-
-def hockey_token
-  ENV['BC_HOCKEY_TOKEN']
-end
-
-def dsym_path_before_zip
-  File.join(ENV['XCS_ARCHIVE'], 'dSYMs', "#{product_name}.app.dSYM")
-end
-
-def dsym_path_after_zip
-  File.join('/tmp', "#{bot_name}.dsym.zip")
-end
-
-def zip_dsym
-  FileUtils.rm_f(dsym_path_after_zip)
-  system("/usr/bin/zip -r \"#{dsym_path_after_zip}\" \"#{dsym_path_before_zip}\"")
-end
-
 puts File.file?(ipa_path)
 puts ipa_path
-puts "zip_dsym = " + zip_dsym
 
-def curl_command
-  command = [
-    '/usr/bin/curl',
-    '-F status=2',
-    '-F notify=0',
-    "-F ipa=@\"#{ipa_path}\"",
-    "-F dsym=@\"#{dsym_path_after_zip}\"",
-    "-H \"X-HockeyAppToken: #{hockey_token}\"",
-    'https://rink.hockeyapp.net/api/2/apps/upload'
-  ]
-  command.join(" ")
-end
+# def bot_name
+#   ENV['XCS_BOT_NAME']
+# end
 
 
-def upload
-  system(curl_command)
-end
+# def product_name
+#   ENV['BC_PRODUCT_NAME']
+# end
 
-raise 'Unable to create dsym.zip.' unless zip_dsym
-raise 'Upload failed.' unless upload
+# def hockey_token
+#   ENV['BC_HOCKEY_TOKEN']
+# end
+
+# def dsym_path_before_zip
+#   File.join(ENV['XCS_ARCHIVE'], 'dSYMs', "#{product_name}.app.dSYM")
+# end
+
+# def dsym_path_after_zip
+#   File.join('/tmp', "#{bot_name}.dsym.zip")
+# end
+
+# def zip_dsym
+#   FileUtils.rm_f(dsym_path_after_zip)
+#   system("/usr/bin/zip -r \"#{dsym_path_after_zip}\" \"#{dsym_path_before_zip}\"")
+# end
+
+# puts File.file?(ipa_path)
+# puts ipa_path
+# puts "zip_dsym = " + zip_dsym
+
+# def curl_command
+#   command = [
+#     '/usr/bin/curl',
+#     '-F status=2',
+#     '-F notify=0',
+#     "-F ipa=@\"#{ipa_path}\"",
+#     "-F dsym=@\"#{dsym_path_after_zip}\"",
+#     "-H \"X-HockeyAppToken: #{hockey_token}\"",
+#     'https://rink.hockeyapp.net/api/2/apps/upload'
+#   ]
+#   command.join(" ")
+# end
+
+
+# def upload
+#   system(curl_command)
+# end
+
+# raise 'Unable to create dsym.zip.' unless zip_dsym
+# raise 'Upload failed.' unless upload
 
