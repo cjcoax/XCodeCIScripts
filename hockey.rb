1637e58f3fa1bb6a6ea9cff3cd59e9e286c8fbd5
Amir Rezvani <arezvani@babycemter.com>
2015-09-23 11:16:08 -0700

    under progress

--- hockey.rb
+++ hockey.rb
@@ -1,9 +1,18 @@
 require 'fileutils'
 
-print ENV['BC_PRODUCT_NAME']
-
 def ipa_path
  File.join(ENV['XCS_OUTPUT_DIR'], ENV['BC_PRODUCT_NAME']) + ".ipa"
 end
 
 print ipa_path 
+
+
+def bot_name
+  ENV['XCS_BOT_NAME']
+end
+
+def product_name
+  ENV['CMD_PRODUCT_NAME']
+end
+
+
