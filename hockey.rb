902d5d30ade74524b9e53e09e53f9675fd3a4d0a
Amir Rezvani <arezvani@babycemter.com>
2015-09-23 11:24:52 -0700

    under progress

--- hockey.rb
+++ hockey.rb
@@ -21,7 +21,40 @@ def dsym_path_before_zip
   File.join(ENV['XCS_ARCHIVE'], 'dSYMs', "#{product_name}.app.dSYM")
 end
 
+def dsym_path_after_zip
+  File.join('/tmp', "#{bot_name}.dsym.zip")
+end
+
+def zip_dsym
+  FileUtils.rm_f(dsym_path_after_zip)
+  system("/usr/bin/zip -r \"#{dsym_path_after_zip}\" \"#{dsym_path_before_zip}\"")
+end
+
+def curl_command
+  command = [
+    '/usr/bin/curl',
+    '-F status=2',
+    '-F notify=0',
+    "-F ipa=@\"#{ipa_path}\"",
+    "-F dsym=@\"#{dsym_path_after_zip}\"",
+    "-H \"X-HockeyAppToken: #{hockey_token}\"",
+    'https://rink.hockeyapp.net/api/2/apps/b6d61934f4af4a45adc56aa5294f7fd6/app_versions/upload'
+  ]
+  command.join(" ")
+end
+
+def upload
+  system(curl_command)
+end
+
+raise 'Unable to create dsym.zip.' unless zip_dsym
+raise 'Upload failed.' unless upload
+
+
+
+
 puts bot_name
 puts ipa_path 
 puts hockey_token
 puts dsym_path_before_zip
+puts dsym_path_after_zip
